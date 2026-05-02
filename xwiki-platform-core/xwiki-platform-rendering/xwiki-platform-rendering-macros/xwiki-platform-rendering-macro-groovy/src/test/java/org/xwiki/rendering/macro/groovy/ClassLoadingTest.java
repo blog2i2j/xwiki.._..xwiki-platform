340d1ef802ae24dceeab5331a5ab2d822c24dce3
Vincent Massol <vincent@massol.net>
2026-05-02 20:00:29 +0200

    [Misc] Add missing assertions

--- xwiki-platform-core/xwiki-platform-rendering/xwiki-platform-rendering-macros/xwiki-platform-rendering-macro-groovy/src/test/java/org/xwiki/rendering/macro/groovy/ClassLoadingTest.java
+++ xwiki-platform-core/xwiki-platform-rendering/xwiki-platform-rendering-macros/xwiki-platform-rendering-macro-groovy/src/test/java/org/xwiki/rendering/macro/groovy/ClassLoadingTest.java
@@ -19,7 +19,10 @@
  */
 package org.xwiki.rendering.macro.groovy;
 
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+
 import java.util.Collections;
+import java.util.Objects;
 
 import org.junit.Test;
 import org.xwiki.rendering.block.MacroBlock;
@@ -38,8 +41,6 @@ import org.xwiki.test.jmock.AbstractComponentTestCase;
  */
 public class ClassLoadingTest extends AbstractComponentTestCase
 {
-    private ScriptMockSetup mockSetup;
-
     private Macro<JSR223ScriptMacroParameters> macro;
     
     private MacroTransformationContext context;
@@ -48,56 +49,56 @@ public class ClassLoadingTest extends AbstractComponentTestCase
     protected void registerComponents() throws Exception
     {
         super.registerComponents();
-        this.mockSetup = new ScriptMockSetup(getComponentManager());
+       new ScriptMockSetup(getComponentManager());
         
         this.macro = getComponentManager().getInstance(Macro.class, "groovy");
         
         this.context = new MacroTransformationContext();
         // The script macro checks the current block (which is a macro block) to see what engine to use
-        this.context.setCurrentMacroBlock(new MacroBlock("groovy", Collections.<String, String>emptyMap(), false));
+        this.context.setCurrentMacroBlock(new MacroBlock("groovy", Collections.emptyMap(), false));
         // Set the syntax since the script macro needs it to parse the script result using that syntax
         this.context.setSyntax(Syntax.XWIKI_2_0);
     }
 
     @Test
-    public void testDefineClassInOneExecutionAndUseInAnother() throws Exception
+    public void defineClassInOneExecutionAndUseInAnother()
     {
         // First execution: define a class
         JSR223ScriptMacroParameters params = new JSR223ScriptMacroParameters();
-        this.macro.execute(params, "class MyClass {}", this.context);
+        assertDoesNotThrow(() -> this.macro.execute(params, "class MyClass {}", this.context));
 
         // Second execution: use the defined class
-        this.macro.execute(params, "def var = new MyClass()", this.context);
+        assertDoesNotThrow(() -> this.macro.execute(params, "def var = new MyClass()", this.context));
     }
 
     @Test
-    public void testExtraJarLocatedAtURL() throws Exception
+    public void extraJarLocatedAtURL()
     {
         // Use a dummy JAR to verify that some passed URL is indeed added to the CL. That JAR only contains
         // an empty Dummy class.
         JSR223ScriptMacroParameters params = new JSR223ScriptMacroParameters();
-        params.setJars(getClass().getClassLoader().getResource("dummy.jar").toString());
+        params.setJars(Objects.requireNonNull(getClass().getClassLoader().getResource("dummy.jar")).toString());
         
         // The test would fail after the next line if the Dummy class wasn't present in the classloader used to 
         // execute the script.
-        this.macro.execute(params, "def var = new Dummy()", this.context);
+        assertDoesNotThrow(() -> this.macro.execute(params, "def var = new Dummy()", this.context));
     }
-    
+
     /**
      * Verify that it's possible to add jars to the class loader used by the Groovy engine
      * across multiple invocations of the Groovy macro. 
      */
     @Test
-    public void testJarParamsInSecondMacro() throws Exception
+    public void jarParamsInSecondMacro()
     {
         JSR223ScriptMacroParameters params = new JSR223ScriptMacroParameters();
 
-        // Execute a first macro without any jars param passed
-        this.macro.execute(params, "def var", this.context);
+        // Execute a first macro, without any jars param passed
+        assertDoesNotThrow(() -> this.macro.execute(params, "def var", this.context));
 
         // Execute a second macro this time with jars param and verify it works
-        params.setJars(getClass().getClassLoader().getResource("dummy.jar").toString());
-        this.macro.execute(params, "def var = new Dummy()", this.context);
+        params.setJars(Objects.requireNonNull(getClass().getClassLoader().getResource("dummy.jar")).toString());
+        assertDoesNotThrow(() -> this.macro.execute(params, "def var = new Dummy()", this.context));
     }
 
     /**
@@ -107,16 +108,18 @@ public class ClassLoadingTest extends AbstractComponentTestCase
      * interface also works.
      */
     @Test
-    public void testDefineClassInFirstExecutionAndJarParamsInAnother() throws Exception
+    public void defineClassInFirstExecutionAndJarParamsInAnother()
     {
-        this.macro.execute(new JSR223ScriptMacroParameters(), "class MyClass {}", this.context);
+        assertDoesNotThrow(
+            () -> this.macro.execute(new JSR223ScriptMacroParameters(), "class MyClass {}", this.context));
 
         // Second execution: use the defined class but with different jar params
         JSR223ScriptMacroParameters params = new JSR223ScriptMacroParameters();
-        params.setJars(getClass().getClassLoader().getResource("dummy.jar").toString());
-        this.macro.execute(params, "def var = new MyClass()", this.context);
+        params.setJars(Objects.requireNonNull(getClass().getClassLoader().getResource("dummy.jar")).toString());
+        assertDoesNotThrow(() -> this.macro.execute(params, "def var = new MyClass()", this.context));
         
         // Third execution without 
-        this.macro.execute(new JSR223ScriptMacroParameters(), "def var = new MyClass()", this.context);
+        assertDoesNotThrow(
+            () -> this.macro.execute(new JSR223ScriptMacroParameters(), "def var = new MyClass()", this.context));
     }
 }
